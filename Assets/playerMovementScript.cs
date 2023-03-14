6b00f1268a8743a7f1ce1bb14462df74306ff7a8
Betzi-afk <ionut.valeriu12@gmail.com>
2023-03-14 12:56:45 +0200

    Initial Commit

---
+++ Assets/playerMovementScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+            
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
